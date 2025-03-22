817f471c8c06bfeb5279603551595b1e596c5ce1
jesus.eduardo369 <jesus.eduardo@2amigos.us>
2025-03-21 23:26:44 -0300

    updated create espacio dto

--- api/src/espacio/dto/create-espacio.dto.ts
+++ api/src/espacio/dto/create-espacio.dto.ts
@@ -1,19 +1,32 @@
-import { IsNotEmpty, IsNumber, IsOptional, IsString } from 'class-validator';
+import {
+  IsEnum,
+  IsNotEmpty,
+  IsNumber,
+  IsOptional,
+  IsString,
+  Max,
+  MaxLength,
+  Min,
+} from 'class-validator';
 
 export class CreateEspacioDto {
   @IsNotEmpty()
   @IsString()
+  @MaxLength(50)
   nombre!: string;
 
   @IsNotEmpty()
-  @IsString()
-  ubicacion!: string;
+  @IsEnum(UbicacionEspacio)
+  ubicacion!: UbicacionEspacio;
 
   @IsNotEmpty()
   @IsNumber()
+  @Min(1)
+  @Max(50)
   capacidad!: number;
 
   @IsOptional()
   @IsString()
+  @MaxLength(200)
   descripcion?: string;
 }
